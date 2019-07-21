81fca91501441c0356b67766626abd116d61ad6b
pawanman56 <pawanman56@gmail.com>
2019-07-21 23:15:04 +0545

    Inital commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Landing from './src/screens/landing';
+
+const App = () => {
+  return(
+    <Landing />
+  )
+}
+
+export default App;
---
+++ src/screens/landing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+const Landing = () => {
+  return(
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fafafa"
+      }}
+    >
+      <Text>Hello World.</Text>
+    </View>
+  )
+}
+
+export default Landing;
