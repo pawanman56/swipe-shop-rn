80e2ebaf1e7f02ee0236411261cbda3776b1a3c0
pawanman56 <pawanman56@gmail.com>
2019-08-01 16:55:06 +0545

    form added

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -1,26 +1,88 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Container, Content, Body, Form, Item, ListItem, Left, Right, Radio, Picker, Icon, Button, Text } from 'native-base';
 
 export default class Landing extends Component {
-  render() {
-    const { container } = styles;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSelected: 'men',
+      pickedValue: undefined
+    };
+  }
 
-    return(
-      <View style={container}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Start Swiping"
-          onPress={() => this.props.navigation.navigate('DeckSwiper')}
-        />
-      </View>
-    )
+  onValueSelected(value: string) {
+    this.setState({
+      isSelected: value
+    });
+  }
+  
+  onValuePicked(value: string) {
+    this.setState({
+      pickedValue: value
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Form>
+
+            <ListItem>
+              <Left>
+                <Text>Men</Text>
+              </Left>
+              <Right>
+                <Radio
+                  selected={(this.state.isSelected === "men")}
+                  value="men"
+                  onPress={this.onValueSelected.bind(this)}
+                />
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text>Women</Text>
+              </Left>
+              <Right>
+                <Radio
+                  selected={(this.state.isSelected === "women")}
+                  value="women"
+                  onPress={this.onValueSelected.bind(this)}
+                />
+              </Right>
+            </ListItem>
+
+            <Item picker>
+              <Picker
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down" />}
+                style={{ width: undefined }}
+                placeholder="You're interested in"
+                placeholderStyle={{ color: "#bfc6ea" }}
+                placeholderIconColor="#007aff"
+                selectedValue={this.state.pickedValue}
+                onValueChange={this.onValuePicked.bind(this)}
+              >
+                <Picker.Item label="Shirts" value="key0" />
+                <Picker.Item label="Pants" value="key1" />
+                <Picker.Item label="Shoes" value="key2" />
+                <Picker.Item label="Beauty & Health Care" value="key3" />
+                <Picker.Item label="Accessories" value="key4" />
+                <Picker.Item label="Seasonial" value="key5" />
+              </Picker>
+            </Item>
+
+            <Button
+              block
+              onPress={() => this.props.navigation.navigate('DeckSwiper')}
+            >
+              <Text>Start Swiping</Text>
+            </Button>
+
+          </Form>
+        </Content>
+      </Container>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
--- src/screens/navigation/appNavigation.js
+++ src/screens/navigation/appNavigation.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import Landing from '../landing';
 import DeckSwiper from '../deckSwiper';
