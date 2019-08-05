a46b56e509a88050ca2699e41a39c1211f6f5c87
pawanman56 <pawanman56@gmail.com>
2019-08-05 17:05:55 +0545

    clean ups

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -7,6 +7,7 @@ import {
   Icon,
   Button,
   Text,
+  View
 } from 'native-base';
 
 import RadioButton from './components/radioButton';
@@ -45,31 +46,39 @@ export default class Landing extends Component {
       <Container>
         <Content style={{ padding: 15 }}>
 
-          <RadioButton options={options} />
+          <View style={{ flex: 1, marginBottom: 15 }}>
+            <Text style={{ fontSize: 24, fontWeight: '700', marginBottom: 7 }}>You are</Text>
+            <RadioButton options={options} />
+          </View>
 
-          <Item picker>
-            <Picker
-              mode="dropdown"
-              iosIcon={<Icon name="arrow-down" />}
-              style={{ width: undefined }}
-              placeholder="You're interested in"
-              placeholderStyle={{ color: "#bfc6ea" }}
-              placeholderIconColor="#007aff"
-              selectedValue={this.state.pickedValue}
-              onValueChange={this.onValuePicked.bind(this)}
-            >
-              <Picker.Item label="Shirts" value="key0" />
-              <Picker.Item label="Pants" value="key1" />
-              <Picker.Item label="Shoes" value="key2" />
-              <Picker.Item label="Beauty & Health Care" value="key3" />
-              <Picker.Item label="Accessories" value="key4" />
-              <Picker.Item label="Seasonial" value="key5" />
-            </Picker>
-          </Item>
+          <View style={{ flex: 1, marginBottom: 15 }}>
+            <Text style={{ fontSize: 24, fontWeight: '700', marginBottom: 7 }}>And you're looking for</Text>
+            
+            <Item picker>
+              <Picker
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down" />}
+                style={{ width: undefined }}
+                placeholder="You're interested in"
+                placeholderStyle={{ color: "#bfc6ea" }}
+                placeholderIconColor="#007aff"
+                selectedValue={this.state.pickedValue}
+                onValueChange={this.onValuePicked.bind(this)}
+              >
+                <Picker.Item label="Shirts" value="key0" />
+                <Picker.Item label="Pants" value="key1" />
+                <Picker.Item label="Shoes" value="key2" />
+                <Picker.Item label="Beauty & Health Care" value="key3" />
+                <Picker.Item label="Accessories" value="key4" />
+                <Picker.Item label="Seasonial" value="key5" />
+              </Picker>
+            </Item>
+          </View>
 
           <Button
             block
             onPress={() => this.props.navigation.navigate('DeckSwiper')}
+            style={{ backgroundColor: '#82589F' }}
           >
             <Text>Start Swiping</Text>
           </Button>
